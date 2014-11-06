80b6f53cdbad980922d089fdee6734d55e9443ad
Sunny Patel <spatel@leadnomics.com>
2014-11-06 09:29:22 -0500

    Adds API for user auth, and changes Dev port to 6550

---
+++ api/models/Auth.js
@@ -0,0 +1,35 @@
+/**
+* Auth.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+
+	email: {
+		type: 'string',
+		unique: true,
+		required: true
+	},
+	password: {
+		type: 'string'
+	},
+	phone: {
+		type: 'string'
+	},
+	accountType: {
+		type: 'integer'
+	},
+	token: {
+		type: 'string'
+	},
+	tokenExpires: {
+		type: 'datetime'
+	}
+
+  }
+};
+
