1d216c0090ca0bf5b6568d17feb16099f04f4c06
Sunny Patel <du.svp28@gmail.com>
2014-12-06 18:50:36 -0500

    Removes Sails version of Manager

--- README.md
+++
@@ -1,3 +0,0 @@
-# JessesManager
-
-a [Sails](http://sailsjs.org) application
--- api/services/Auth.js
+++
@@ -1,7 +0,0 @@
-// Authenication Service
-
-module.exports = {
-  doLogin: function (req, res) {
-    // THIS SHOULD REALLY BE HANDLING ALL THE LOGIN STUFF
-  }
-}
