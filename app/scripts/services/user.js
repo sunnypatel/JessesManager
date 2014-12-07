4d56720f200458ae56043201a14b6380b31d5908
Sunny Patel <du.svp28@gmail.com>
2014-12-07 10:16:21 -0500

    Finished Login UI

---
+++ app/scripts/services/user.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+* @ngdoc function
+* @name jessesManager2App.controller:AboutCtrl
+* @description
+* # AboutCtrl
+* Controller of the jessesManager2App
+*/
+angular.module('jessesManager2App')
+.controller('userService', function ($scope) {
+
+});
