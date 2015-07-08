17bd9afa10afa09ea82510c07dc391493350c28d
Sunny Patel <du.svp28@gmail.com>
2015-07-07 23:36:14 -0400

    Adds updated staging url to connections service and io.sails

--- app/scripts/services/connections.js
+++ app/scripts/services/connections.js
@@ -11,10 +11,10 @@ angular.module('jessesManager2App')
 .factory('ConnectionsService', ['$http', '$window', function($http, $window) {
     var apiToken = $window.sessionStorage.apiToken;
     var dev = 'http://localhost:2730';
-    var staging = 'http://178.18.16.226:2730';
+    var staging = 'http://stagingjesseme.ddns.net:4830';
     var prod = 'http://restaurantapi.jesseme.com';
 
-    var url = prod;
+    var url = staging;
     var factory = {};
 
     factory.getUrl = function() {
